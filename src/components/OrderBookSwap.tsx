18ede34ee6de08cdd104dabfb7e961999c1fca3b
MrROIInu <roiinucoin@gmail.com>
2024-12-03 18:43:26 +0200

    Update P2PSwap implementation

--- src/components/OrderBookSwap.tsx
+++ src/components/OrderBookSwap.tsx
@@ -5,6 +5,7 @@ import { TransactionHistory } from './TransactionHistory';
 import { useOrders } from '../hooks/useOrders';
 import { useSwapContext } from '../contexts/SwapContext';
 import { SelectedTokenChart } from './charts/SelectedTokenChart';
+import { CollectionChart } from './CollectionChart';
 
 export const OrderBookSwap: React.FC = () => {
   const { orders, loading, error, onClaim, onCancel, fetchOrders } = useOrders();
@@ -29,13 +30,15 @@ export const OrderBookSwap: React.FC = () => {
         )}
         
         <SelectedTokenChart />
-
+        
         <OrderList
           orders={filteredOrders}
           onCancel={onCancel}
           onClaim={onClaim}
           loading={loading}
         />
+
+        <CollectionChart />
 
         <TransactionHistory 
           transactions={[]}
