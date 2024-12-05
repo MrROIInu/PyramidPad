d44374530729475e2e9f4cef662e82dfc8d665ab
MrROIInu <roiinucoin@gmail.com>
2024-12-05 19:59:44 +0200

    Update P2PSwap implementation

--- src/pages/TokenList.tsx
+++ src/pages/TokenList.tsx
@@ -18,7 +18,7 @@ export const TokenList: React.FC = () => {
         RXD20 Token List
       </h1>
       
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="space-y-6">
         {sortedTokens.map(token => (
           <TokenCard key={token.symbol} token={token} />
         ))}
